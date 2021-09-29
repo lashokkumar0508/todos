b769694b5898d8f34f07264fade5483da8a987e3
lashokkumar0508 <lashokkumar0508@gmail.com>
2021-09-29 19:56:06 +0530

    Can see single todo in todo/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #RESOURCE
+  #Todo is a Resource
+  # -- List of respurces can be see : index
+  # -- Single Resource can be seen : show
+  # -- Create Resources
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 end
