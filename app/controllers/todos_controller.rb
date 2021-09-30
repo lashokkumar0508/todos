ec008036b200d41be8f203e239d5a1a8f9161e02
lashokkumar0508 <lashokkumar0508@gmail.com>
2021-09-30 07:07:03 +0530

    add update option in todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,7 +15,13 @@ class TodosController < ApplicationController
     new_todo = Todo.create!(todo_text:todo_text, due_date:due_date, completed:false)
     respose_text = "Hey you create todo wit ID = #{new_todo.id}"
     render plain: respose_text
-
-
+  end
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "The updated to status #{completed}"
   end
 end
