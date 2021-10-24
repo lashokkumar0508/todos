017c60e20281a808fd69896fb0ca37b54535d35d
lashokkumar0508 <lashokkumar0508@gmail.com>
2021-10-24 21:05:00 +0530

    has_secure_password to password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   skip_before_action :verify_authenticity_token
   def index
     #render plain: "Welcom to user all"
@@ -7,8 +8,8 @@ class UsersController < ApplicationController
   def create
     name = params[:first_name]
     email = params[:email]
-    password = params[:password]
-    new_user = User.create!(first_name: name, last_name: params[:last_name],email: email,password_digest: password)
+    params[:password]
+    new_user = User.create!(first_name: name, last_name: params[:last_name],email: email,password: params[:password])
     respose_text = "Hey you create todo wit ID = #{new_user.id}"
     render plain: respose_text
   end
@@ -17,6 +18,9 @@ class UsersController < ApplicationController
     password = params[:password]
     user = User.where(["email = ? and password = ?", email, password]).exists?
     render plain: user
+  end
+  def show
+    render plain:User.all.map {|user| user.display_user }.join("\n")
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :todos
   def display_user
-    "#{first_name} #{email}  #{password_digest}"
+    "#{first_name} #{last_name} #{email}  #{password_digest}"
   end
 
 end
